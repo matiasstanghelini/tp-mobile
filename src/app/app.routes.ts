cdf2c0bd5924c3e5e17a620b802d869cfdbf28ff
matias <stanghelinimatias@gmail.com>
2025-06-30 20:04:57 -0300

    feat(routing): configurar rutas principales
    - Definir rutas principales
    - Configurar carga perezosa
    - Establecer ruta por defecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'today',
+    loadComponent: () => import('./pages/today/today.page').then( m => m.TodayPage)
+  },
+  {
+    path: 'habits',
+    loadComponent: () => import('./pages/habits/habits.page').then( m => m.HabitsPage)
+  },
+  {
+    path: 'stats',
+    loadComponent: () => import('./pages/stats/stats.page').then( m => m.StatsPage)
+  },
 ];
