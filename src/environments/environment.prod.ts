8634bb1a984fca490e5c76f04c872d5ca81620b4
matias <stanghelinimatias@gmail.com>
2025-06-30 23:09:28 -0300

    Actualize el package y cree ambientes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // Aquí puedes agregar las variables de entorno de producción
+  apiUrl: 'http://localhost:8100',
+  // Agrega más variables según sea necesario
+};
